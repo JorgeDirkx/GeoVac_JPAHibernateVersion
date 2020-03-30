27fc99a4fe30816b32ec8cc96ca1aaaa9ccc72a2
JorgeDirkx <jorge.dirkx@gmail.com>
2020-03-30 21:13:05 +0200

    @Entity Class Client

--- src/main/java/com/jorgedirkx/Client.java
+++ src/main/java/com/jorgedirkx/Client.java
@@ -1,13 +1,69 @@
 package com.jorgedirkx;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
-
+@Entity
+@Table (name = "client")
 public class Client {
 
+    @Id @GeneratedValue
     private int id;
     private String name;
     private Date dob;
     private String passportNr;
     private String gender;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getPassportNr() {
+        return passportNr;
+    }
+
+    public void setPassportNr(String passportNr) {
+        this.passportNr = passportNr;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dob=" + dob +
+                ", passportNr='" + passportNr + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
 }
